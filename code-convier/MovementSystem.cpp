4008eaf3aa302015f1f5956a361d46b4cce021b9
Joel Tio <joeltiojinhon@gmail.com>
2020-02-11 11:34:59 +0800

    Add physics system

--- code-convier/MovementSystem.cpp
+++
@@ -1,9 +0,0 @@
-#include "PlayerMovementSystem.h"
-
-namespace System {
-
-	void PlayerMovement::update(float frameTime)
-	{
-		// update the player movement
-	}
-}
--- code-convier/ecsConstants.h
+++ code-convier/ecsConstants.h
@@ -3,10 +3,12 @@
 #include "System.h"
 
 #include "FiniteStateSystem.h"
+#include "PhysicsSystem.h"
 #include "RenderSystem.h"
 
 // Systems to use
 static ECS::System* ENABLED_SYSTEMS[] = {
 	new System::FiniteState(),
+	new System::Physics(),
 	new System::Render(),
 };
