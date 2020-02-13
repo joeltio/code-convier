0782e7e3370dfa8c6ceeb15de7ae45d28ab19e57
Jiale Lim <s10184594@connect.np.edu.sg>
2020-02-13 21:39:27 +0800

    added attack component for player

---
+++ code-convier/AttackComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Component.h"
+
+namespace Component {
+
+	struct Attack : public ECS::Component {
+	public:
+		int attackDamage = 0;
+		float attackCooldownTimer = 0.0f;
+		bool multiHits = false;
+		int chargeDamage = 0;
+	};
+
+}
--- code-convier/AugmentationSystem.cpp
+++ code-convier/AugmentationSystem.cpp
@@ -1,1 +1,20 @@
 #include "AugmentationSystem.h"
+
+namespace System {
+
+	void Augmentation::update(float frameTime)
+	{
+		// get the id of the player entity
+		std::unordered_set<ECS::EntityIdType>* playerPtrs = manager->getEntities<Entity::Player>();
+
+		for (ECS::EntityIdType id : *playerPtrs)
+		{
+			Component::Augmentation augmentationComponent = manager->getEntityComponent<Component::Augmentation>(id);
+			Component::Health healthComponent = manager->getEntityComponent<Component::Health>(id);
+			Component::Physics physicsComponent = manager->getEntityComponent<Component::Physics>(id);
+			Component::Attack attackComponent = manager->getEntityComponent<Component::Attack>(id);
+		}
+	}
+
+}
+
