593b4da7ce2894d68d369307474d0fd81dff554c
Joel Tio <joeltiojinhon@gmail.com>
2020-02-16 01:42:09 +0800

    Optimise vectorUtil to use references

--- code-convier/vectorUtil.h
+++ code-convier/vectorUtil.h
@@ -8,4 +8,4 @@ char getQuadrant(D3DXVECTOR2& subject, D3DXVECTOR2& reference);
 char getQuadrant(D3DXVECTOR2& subject, D3DXVECTOR2& reference, D3DXVECTOR2& axis);
 float distance2(D3DXVECTOR2& a, D3DXVECTOR2& b);
 bool doExtentsIntersect(std::pair<float, float>& a, std::pair<float, float>& b);
-std::pair<float, float> findVectorExtents(std::vector<D3DXVECTOR2> corners, D3DXVECTOR2 normal);
+std::pair<float, float> findVectorExtents(std::vector<D3DXVECTOR2>& corners, D3DXVECTOR2& normal);
