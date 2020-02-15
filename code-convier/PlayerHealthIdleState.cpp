6c6f0fcb23c60ac68f99470f836efb009cafe58f
Wei Le Jabier Ho <s10187854@connect.np.edu.sg>
2020-02-16 00:01:56 +0800

    healthcomponent removed

--- code-convier/PlayerHealthIdleState.cpp
+++ code-convier/PlayerHealthIdleState.cpp
@@ -5,13 +5,12 @@ FSM::Action PlayerHealthIdleState::update(float frametime, Component::State stat
 	std::unordered_set<ECS::EntityIdType> playerEntityIds = *this->manager->getEntities<Entity::Player>();
 	for (ECS::EntityIdType playerEntityId : playerEntityIds)
 	{
-		Component::Health healthComponent = manager->getEntityComponent<Component::Health>(playerEntityId);
+		Component::Health& healthComponent = manager->getEntityComponent<Component::Health>(playerEntityId);
 		if (healthComponent.health <= 0) {
 			return PreparePlayerDeathAction();
 		}
 
 		// reduce the health of the player
-		Component::Health& healthComponent = manager->getEntityComponent<Component::Health>(playerEntityId);
 		healthComponent.health -= HEALTH_TICK;
 	}
 	return FSM::NoAction();
