22fa4a89e3a1ffe75332eb23d3c50697611bf380
Jiale Lim <s10184594@connect.np.edu.sg>
2020-02-16 16:25:39 +0800

    finished player attack logic

--- code-convier/PlayerAttackingState.cpp
+++ code-convier/PlayerAttackingState.cpp
@@ -1,12 +1,55 @@
 #include "PlayerAttackingState.h"
 
 FSM::Action PlayerAttackingState::update(float frameTime, Component::State stateComponent) {
+
 	// Get the direction the entity is facing, using right as the benchmark
-	ECS::EntityIdType playerId = *manager->getEntities<Entity::Player>()->begin();
-	bool facingRight = manager->getEntityComponent<Component::Transform>(playerId).flipHorizontal;
+	ECS::EntityIdType playerId = stateComponent.entityId;
+	Component::Transform transformComponent = manager->getEntityComponent<Component::Transform>(playerId);
+	Component::Texture textureComponent = manager->getEntityComponent<Component::Texture>(playerId);
+	Component::Animatable animatableComponent = manager->getEntityComponent<Component::Animatable>(playerId);
+	Component::Attack attackComponent = manager->getEntityComponent<Component::Attack>(playerId);
+	int attackDirectionSign = (transformComponent.flipHorizontal) ? 1 : -1;
 
-	// Create the hitboxes
+	// Create the hitboxes, taking into account the direction
+	Component::HurtBox hitBoxComponent = Component::HurtBox();
+	float playerWidth = textureComponent.totalWidth / animatableComponent.columns * transformComponent.scale;
+	float playerHeight = textureComponent.totalHeight / animatableComponent.rows * transformComponent.scale;
 
+	// Set the corners of the hitbox
+	hitBoxComponent.corners.push_back(D3DXVECTOR2(
+		transformComponent.x + playerWidth / 2, 
+		transformComponent.y
+	));
 
-	return FSM::NoAction();
+	hitBoxComponent.corners.push_back(D3DXVECTOR2(
+		transformComponent.x + playerWidth / 2,
+		transformComponent.y + playerHeight
+	));
+	
+	hitBoxComponent.corners.push_back(D3DXVECTOR2(
+		transformComponent.x + playerWidth / 2 + attackComponent.range * attackDirectionSign,
+		transformComponent.y
+	));
+
+	hitBoxComponent.corners.push_back(D3DXVECTOR2(
+		transformComponent.x + playerWidth / 2 + attackComponent.range * attackDirectionSign,
+		transformComponent.y + playerHeight
+	));
+
+	hitBoxComponent.onEnter = [playerId, attackComponent](ECS::Manager* manager, ECS::EntityIdType id) {
+		// check for collision with enemy types and deal damage to them
+		if (manager->getEntity(id)->isSameType<Entity::Enemy>())
+		{
+			manager->getEntityComponent<Component::Health>(id).health -= attackComponent.damage;
+			if (!attackComponent.multiHits)
+			{
+				manager->removeComponent<Component::HurtBox>(playerId);
+			}
+		}
+	};
+
+	// reset the attack timer
+	attackComponent.cooldownTimer = attackComponent.cooldown;
+
+	return PlayerAttackingIdle();
 }
