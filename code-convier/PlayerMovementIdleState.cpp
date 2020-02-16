f2a4b82ec26ecd7f9daa5bf221b0385dd7f2448d
Jiale Lim <s10184594@connect.np.edu.sg>
2020-02-16 22:14:30 +0800

    added comments

--- code-convier/PlayerMovementIdleState.cpp
+++ code-convier/PlayerMovementIdleState.cpp
@@ -1,5 +1,6 @@
 #include "PlayerMovementIdleState.h"
 
 FSM::Action PlayerMovementIdleState::update(float frameTime, Component::State stateComponent) {
+	// paused state 
 	return FSM::NoAction();
 }
